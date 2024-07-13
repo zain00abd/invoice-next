a5ea39dd5d5ca04e58d870e39c9dc51ae4dc09ee
zain00abd <zainabflash1998@gmail.com>
2024-07-13 06:22:06 +0300

    add sining page

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,39 @@
+// import Footer from "components/footer/footer";
+// import Header from "components/header/header";
+
+import "../style.css";
+
+export const metadata = {
+  title: "Register page",
+  description: "description for Register page",
+};
+
+const Page = () => {
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <h1> انشاء حساب </h1>
+        <form>
+          <div className="form-group">
+            <input type="text" id="username" placeholder=" الاسم "/>
+
+            <div className="bar" />
+          </div>
+          <div className="form-group">
+            <input type="email" id="email" placeholder=" البريد الالكتروني "/>
+
+            <div className="bar" />
+          </div>
+          <div className="form-group">
+            <input type="password" id="password" placeholder="كلمة المرور"/>
+
+            <div className="bar" />
+          </div>
+          <button type="submit">تسجيل</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/(auth)/sining/page.jsx
@@ -0,0 +1,37 @@
+
+
+
+import '../style.css'
+
+export const metadata = {
+  title: "Signin page",
+  description: "description for Signin page",
+};
+const Page = () => {
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <h1> تسجيل الدخول </h1>
+        <form>
+          <div className="form-group">
+
+            <div className="bar" />
+          </div>
+          <div className="form-group">
+            <input type="email" id="email" placeholder=" البريد الالكتروني "/>
+
+            <div className="bar" />
+          </div>
+          <div className="form-group">
+            <input type="password" id="password" placeholder="كلمة المرور"/>
+
+            <div className="bar" />
+          </div>
+          <button type="submit"> تسجيل الدخول </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
