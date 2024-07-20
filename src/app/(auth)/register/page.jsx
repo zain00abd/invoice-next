a49b5f24281f881ad0f76018f5a14d6b8b4d8e6e
zain00abd <zainabflash1998@gmail.com>
2024-07-20 07:04:53 +0300

    add .suggestion Link auth

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,7 +1,9 @@
 // import Footer from "components/footer/footer";
 // import Header from "components/header/header";
 
-import Head from "components/Head";
+import Link from "next/link";
+
+
 
 
 export const metadata = {
@@ -14,7 +16,7 @@ const Page = () => {
     
     
 <>
-        <Head actev={"sdwe"} />
+
       <div className="login-container">
         <div className="login-form">
           <h1> انشاء حساب </h1>
@@ -31,9 +33,17 @@ const Page = () => {
             </div>
             <div className="form-group">
               <input type="password" id="password" placeholder="كلمة المرور"/>
-  
+
+              
               <div className="bar" />
             </div>
+              <div className="suggestion">
+                <p>
+                  {" "}
+                {" "}هل لديك حساب بالفعل؟{" "}
+                  <Link href={"/sining"}> تسجيل الدخول </Link>
+                </p>
+              </div>
             <button type="submit">تسجيل</button>
           </form>
         </div>
--- src/app/(auth)/sining/page.jsx
+++ src/app/(auth)/sining/page.jsx
@@ -1,8 +1,4 @@
-
-
-
-import Head from 'components/Head';
-
+import Link from "next/link";
 
 export const metadata = {
   title: "Signin page",
@@ -11,31 +7,40 @@ export const metadata = {
 
 const Page = () => {
   return (
-<>
-      <Head actev={"sdwe"}/>
+    <>
       <div className="login-container">
         <div className="login-form">
           <h1> تسجيل الدخول </h1>
           <form>
             <div className="form-group">
-  
               <div className="bar" />
             </div>
             <div className="form-group">
-              <input type="email" id="email" placeholder=" البريد الالكتروني "/>
-  
+              <input
+                type="email"
+                id="email"
+                placeholder=" البريد الالكتروني "
+              />
+
               <div className="bar" />
             </div>
             <div className="form-group">
-              <input type="password" id="password" placeholder="كلمة المرور"/>
-  
+              <input type="password" id="password" placeholder="كلمة المرور" />
               <div className="bar" />
             </div>
+              <div className="suggestion">
+                <p>
+                  {" "}
+                  اذا لم يكن لديك حساب{" "}
+                  <Link href={"/register"}> انشاء حساب </Link>
+                </p>
+              </div>
+
             <button type="submit"> تسجيل الدخول </button>
           </form>
         </div>
       </div>
-</>
+    </>
   );
 };
 
